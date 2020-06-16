7b404405b74c3c061d60d59f401390b62301e33f
andy <andy@ryinfotech.com>
2020-06-16 14:43:11 +0800

    新增 prerender-spa-plugin 插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,18 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+
 module.exports = (config, { webpack }) => {
+  const plugins = [
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /de|fr|hu/),
+    new PrerenderSPAPlugin({
+      staticDir: path.join(__dirname, 'dist'),
+      outputDir: path.join(__dirname, 'dist/prerender'),
+      routes: [ '/home', '/base64', '/qrcode' ],
+    })
+  ];
+
   // eslint-disable-next-line
-  config.plugins.push(new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /de|fr|hu/));
+  config.plugins.push(...plugins);
   return config;
-}
+};
