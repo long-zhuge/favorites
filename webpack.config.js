b480eba145607f3bcf940022a0575266fb8abac3
andy <andy@ryinfotech.com>
2020-08-07 11:38:06 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
+// const PrerenderSPAPlugin = require('prerender-spa-plugin');
 // const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
 module.exports = (config, { webpack }) => {
   const plugins = [
     new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /de|fr|hu/),
-    new PrerenderSPAPlugin({
-      staticDir: path.join(__dirname, 'dist'),
-      outputDir: path.join(__dirname, 'dist/prerender'),
-      routes: [ '/home', '/base64', '/qrcode' ],
-    })
+    // new PrerenderSPAPlugin({
+    //   staticDir: path.join(__dirname, 'dist'),
+    //   outputDir: path.join(__dirname, 'dist/prerender'),
+    //   routes: [ '/home', '/base64', '/qrcode' ],
+    // })
   ];
 
   // eslint-disable-next-line
