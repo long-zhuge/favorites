759e502b51bb3a379c0c3aac2911c56db046664f
andy <andy@ryinfotech.com>
2020-11-27 17:26:02 +0800

    update

--- src/layouts/GlobalHeader/index.js
+++ src/layouts/GlobalHeader/index.js
@@ -19,30 +19,25 @@ export default (props) => {
 
   return (
     <div className={styles.container}>
-      <ContentBox>
-        <div className={styles.header}>
-          <div className={styles.logo}>
-            FAVORITES
-          </div>
-          <Menu
-            className={styles.menu}
-            onClick={handleClick}
-            selectedKeys={[menuKey]}
-            mode="horizontal"
-          >
-            <Menu.Item key="/home">主页</Menu.Item>
-            <Menu.Item key="/base64">base64</Menu.Item>
-            {/*<Menu.Item key="/favorites/excel">excel</Menu.Item>*/}
-            <Menu.Item key="/qrcode">二维码</Menu.Item>
-            <Menu.Item key="/pdf">PDF</Menu.Item>
-            <Menu.Item key="/signature">签名</Menu.Item>
-            <Menu.Item key="/forge">加密</Menu.Item>
-            {/*<Menu.Item key="/favorites/markdown">markdown</Menu.Item>*/}
-            {/*<Menu.Item key="/favorites/chiHuang">图片鉴黄</Menu.Item>*/}
-          </Menu>
-          <BgMusic />
-        </div>
-      </ContentBox>
+      <div className={styles.title}>FAVORITES</div>
+      <div className={styles.menus}>
+        <Menu
+          className={styles.menu}
+          onClick={handleClick}
+          selectedKeys={[menuKey]}
+          mode="horizontal"
+        >
+          <Menu.Item key="/home">主页</Menu.Item>
+          <Menu.Item key="/base64">base64</Menu.Item>
+          {/*<Menu.Item key="/favorites/excel">excel</Menu.Item>*/}
+          <Menu.Item key="/qrcode">二维码</Menu.Item>
+          <Menu.Item key="/pdf">PDF</Menu.Item>
+          <Menu.Item key="/signature">签名</Menu.Item>
+          <Menu.Item key="/forge">加密</Menu.Item>
+          {/*<Menu.Item key="/favorites/markdown">markdown</Menu.Item>*/}
+          {/*<Menu.Item key="/favorites/chiHuang">图片鉴黄</Menu.Item>*/}
+        </Menu>
+      </div>
     </div>
   );
 }
